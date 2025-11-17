29a70e00df8bab50958cfcbc2f11a2d4321710e1
pyuvaraj37 <ryuvaraj37@gmail.com>
2025-11-17 12:46:06 -0800

    Added vitis_hls testbench

---
+++ fpga/bp/test_bp.cpp
@@ -0,0 +1,22 @@
+#include "include/bp.hpp"
+
+
+
+int main(int argc, char** argv) {
+
+
+    /*
+    
+        TODO: Add testbench code here to validate the krnl_bp function.
+
+        SETUP INPUT 
+
+        CALL KERNEL
+
+        CHECK OUTPUT
+    
+    */
+    krnl_bp(/* appropriate arguments */);
+
+    return 0;
+}

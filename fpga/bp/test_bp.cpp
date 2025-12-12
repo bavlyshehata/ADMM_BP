24d31b678db9e90e1e1ef5e0a6816f5d1b9a7da4
Bavly Shehata <110245724+bavlyshehata@users.noreply.github.com>
2025-12-12 15:19:05 -0800

    Implemented a testbench for ADMM BP implementation

--- fpga/bp/test_bp.cpp
+++ fpga/bp/test_bp.cpp
@@ -1,22 +1,76 @@
 #include "include/bp.hpp"
+#include <cmath>
+#include <iostream>
 
-
+static bool approxEqual(data_t a, data_t b, float tol = 1e-2f){
+    return std::fabs(a-b) < tol;
+}
 
 int main(int argc, char** argv) {
-
-
-    /*
+    const int N = 512;
     
-        TODO: Add testbench code here to validate the krnl_bp function.
+    data_t P[N * N];
+    data_t q[N];
+    data_t x_out[N];
+    data_t z[N];
+    data_t u[N];
+    data_t zold[N];
 
-        SETUP INPUT 
 
-        CALL KERNEL
+// Identity Matrix P
+    for (int i = 0; i < N * N; i++) {
+        P[i] = 0.0f;
+    }
+    for (int i = 0; i < N; i++) {
+        P[i * N + i] = 1.0f;
+    } 
 
-        CHECK OUTPUT
+// Expected Solution, sets q equal to our solution
+    data_t expected[N] = {1.0f, -0.5f, 0.0f, 2.0f, -1.0f, 0.75f, 0.0f, -0.25f};
+
+    for (int i = 0; i < N; i++) {
+        q[i] = expected[i];
+    }
+
+//Initialization
+    for (int i = 0; i < N; i++) {
+        x_out[i] = 0.0f;
+        z[i]     = 0.0f;
+        u[i]     = 0.0f;
+        zold[i]  = 0.0f;
+    }
     
-    */
-    krnl_bp(/* appropriate arguments */);
+    data_t rho        = 1.0f;
+    data_t alpha      = 1.0f;
+    bool   terminate  = true;
+    int    MAX_ITER   = 1;
 
-    return 0;
+//Calling The Kernel
+    krnl_bp(P, q, x_out, z, u, zold, rho, alpha, N, terminate, MAX_ITER);
+
+//Check Output
+    std::cout << "Expected: ";
+    for (int i = 0; i < N; i++) std::cout << expected[i] << " ";
+    std::cout << "\n";
+
+    std::cout << "Kernel: ";
+    for (int i = 0; i < N; i++) std::cout << x_out[i] << " ";
+    std::cout << "\n";
+
+    bool pass = true;
+
+    for (int i = 0; i < N; i++) {
+        if (!approxEqual(x_out[i], expected[i], 1e-2f)) {
+            pass = false;
+            std::cout << "index " << i << " wrong, expected: " << expected[i] << " actual: " << x_out[i] << "\n";
+        }
+    }
+
+    if (pass) {
+        std::cout << "Pass!\n";
+        return 0;
+    } else {
+        std::cout << "Fail :(\n";
+        return 1;
+    }
 }
